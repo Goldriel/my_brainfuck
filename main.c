00327fd4769dd608523b321b9027555b6a4a54dc
Janette Arrakis <jarrakis@vo-l7.msk.21-school.ru>
2021-09-20 19:22:57 +0300

    ok

--- main.c
+++ main.c
@@ -6,23 +6,47 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 15:01:18 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/09/20 16:43:46 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/09/20 19:22:25 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 #include<stdlib.h>
 
+void	ft_print_char(char c)
+{
+	write(1, &c, 1);
+}
+
 void	brainfuck(char *str)
 {
 	char	*buff;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	buff = (char *)malloc(sizeof(char) * 30000);
 	if (buff == NULL)
 	{
 		write(1, "Buff Erorr", 10);
-		return (NULL);
+		return ;
 	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == '>')
+			j++;
+		if (str[i] == '<')
+			j--;
+		if (str[i] == '+')
+			buff[j]++;
+		if (str[i] == '-')
+			buff[j]--;
+		if (str[i] == '.')
+			ft_print_char(buff[j]); // допилить циклы
+		i++;
+	}
+	free(buff);
 }
 
 int	main(int argc, char **argv)
@@ -30,7 +54,7 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 	{
 		write(1, "argument Erorr", 14);
-		return (NULL);
+		return (0);
 	}
 	else if (argc == 2)
 	{
@@ -39,6 +63,6 @@ int	main(int argc, char **argv)
 	else if (argc >= 3)
 	{
 		write(1, "To many argument", 16);
-		return (NULL);
+		return (0);
 	}
 }
