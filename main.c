6139730cbb8b251a40780e1fc1ea1a066ae8ef3a
Janette Arrakis <jarrakis@vo-l7.msk.21-school.ru>
2021-09-20 16:45:28 +0300

    input analysis ready

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 15:01:18 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/09/20 16:33:42 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/09/20 16:43:46 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,18 @@ void	brainfuck(char *str)
 
 int	main(int argc, char **argv)
 {
-	if ()
+	if (argc == 1)
 	{
+		write(1, "argument Erorr", 14);
+		return (NULL);
+	}
+	else if (argc == 2)
+	{
+		brainfuck(argv[1]);
+	}
+	else if (argc >= 3)
+	{
+		write(1, "To many argument", 16);
+		return (NULL);
 	}
 }
